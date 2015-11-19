ce0271d81a166ef66e66f6d16e287d5c5e8fb947
Misha Kharaba <misha.kharaba@gmail.com>
2015-11-19 23:42:26 +0200

    Show check time and error

--- app/src/main/java/com/eleks/mailwatcher/AlertListAdapter.java
+++ app/src/main/java/com/eleks/mailwatcher/AlertListAdapter.java
@@ -3,6 +3,7 @@ package com.eleks.mailwatcher;
 import android.content.Context;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,51 +16,43 @@ import com.eleks.mailwatcher.model.AlertModel;
 
 import java.util.List;
 
-public class AlertListAdapter extends BaseAdapter
-{
+public class AlertListAdapter extends BaseAdapter {
     private final Context mContext;
     private List<AlertModel> mAlerts;
 
-    public AlertListAdapter(Context context, List<AlertModel> alerts)
-    {
+    public AlertListAdapter(Context context, List<AlertModel> alerts) {
         mContext = context;
         mAlerts = alerts;
     }
 
-    public void setAlerts(List<AlertModel> alerts)
-    {
+    public void setAlerts(List<AlertModel> alerts) {
         mAlerts = alerts;
     }
 
     @Override
-    public int getCount()
-    {
+    public int getCount() {
         if (mAlerts != null)
             return mAlerts.size();
         return 0;
     }
 
     @Override
-    public Object getItem(int position)
-    {
+    public Object getItem(int position) {
         if (mAlerts != null)
             return mAlerts.get(position);
         return null;
     }
 
     @Override
-    public long getItemId(int position)
-    {
+    public long getItemId(int position) {
         if (mAlerts != null)
             return mAlerts.get(position).id;
         return 0;
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup parent)
-    {
-        if (view == null)
-        {
+    public View getView(int position, View view, ViewGroup parent) {
+        if (view == null) {
             LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = inflater.inflate(R.layout.alert_list_item, parent, false);
         }
@@ -79,33 +72,48 @@ public class AlertListAdapter extends BaseAdapter
         TextView txtLabel = (TextView) view.findViewById(R.id.alert_label_name);
         txtLabel.setText(alert.labelName);
 
+        TextView txtLastChecked = (TextView) view.findViewById(R.id.last_checked);
+        java.text.DateFormat df = DateFormat.getDateFormat(view.getContext());
+        java.text.DateFormat tf = DateFormat.getTimeFormat(view.getContext());
+        if (alert.lastCheckDate != null && alert.historyId != null) {
+            String msg = df.format(alert.lastCheckDate) + " " + tf.format(alert.lastCheckDate);
+            txtLastChecked.setText(msg);
+            txtLastChecked.setVisibility(View.VISIBLE);
+        } else {
+            txtLastChecked.setText("");
+            txtLastChecked.setVisibility(View.GONE);
+        }
+
+        TextView txtError = (TextView) view.findViewById(R.id.error);
+        if (alert.lastError != null && alert.isEnabled) {
+            txtError.setText(alert.lastError);
+            txtError.setVisibility(View.VISIBLE);
+        } else {
+            txtError.setText("");
+            txtError.setVisibility(View.GONE);
+        }
+
         ToggleButton btnEnabled = (ToggleButton) view.findViewById(R.id.alert_item_toggle);
         btnEnabled.setChecked(alert.isEnabled);
         btnEnabled.setTag(alert.id);
-        btnEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
+        btnEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 ((AlertListActivity) mContext).setAlertEnabled((Long) buttonView.getTag(), isChecked);
             }
         });
 
         view.setTag(alert.id);
-        view.setOnClickListener(new View.OnClickListener()
-        {
+        view.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 ((AlertListActivity) mContext).startAlertDetailsActivity((Long) view.getTag());
             }
         });
 
-        view.setOnLongClickListener(new View.OnLongClickListener()
-        {
+        view.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public boolean onLongClick(View view)
-            {
+            public boolean onLongClick(View view) {
                 ((AlertListActivity) mContext).deleteAlarm((Long) view.getTag());
                 return true;
             }
